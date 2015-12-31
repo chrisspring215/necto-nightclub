8d4943cd63d2168daff30b02cd47df462a094d68
Christopher Collins <chris@spring215.com>
2015-12-30 19:23:42 -0500

    Added booth rental date to the form

--- reservation_contact_from.php
+++ reservation_contact_from.php
@@ -3,11 +3,12 @@
 $reservation_name = $_POST['reservation_name'];
 $reservation_phone = $_POST['reservation_phone'];
 $reservation_email = $_POST['reservation_email'];
+$reservation_date = $_POST['reservation_date'];
 $reservation_message = $_POST['reservation_message'];
 $to = 'info@thenecto.com';
 $subject = 'Booth Rental or Bottle Service';
 
-$message = 'NAME: ' . $reservation_name . "\n" . 'PHONE: ' . $reservation_phone . "\n" . 'EMAIL: ' . $reservation_email . "\n" . 'MESSAGE: ' . $reservation_message;
+$message = 'NAME: ' . $reservation_name . "\n" . 'PHONE: ' . $reservation_phone . "\n" . 'EMAIL: ' . $reservation_email . "\n" . $reservation_date . "\n" . 'MESSAGE: ' . $reservation_message;
 
 mail ($to, $subject, $message, 'From: ' . $reservation_name);
 header("Location: http://www.necto.com/bottle-service-and-reservation-thank-you.html");
