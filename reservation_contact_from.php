749fd6de448335e6d0f8203d54ef0cff6021bcc8
Christopher Collins <chris@spring215.com>
2016-06-01 18:30:36 -0400

    reservations forms

--- reservation_contact_from.php
+++ reservation_contact_from.php
@@ -5,12 +5,14 @@ $reservation_phone = $_POST['reservation_phone'];
 $reservation_email = $_POST['reservation_email'];
 $reservation_date = $_POST['reservation_date'];
 $reservation_message = $_POST['reservation_message'];
-$to = 'info@thenecto.com';
+// $to = 'info@thenecto.com';
+$to = 'info@necto.com';
 $subject = 'Booth Rental or Bottle Service';
 
 $message = 'NAME: ' . $reservation_name . "\n" . 'PHONE: ' . $reservation_phone . "\n" . 'EMAIL: ' . $reservation_email . "\n" . $reservation_date . "\n" . 'MESSAGE: ' . $reservation_message;
 
-mail ($to, $subject, $message, 'From: ' . $reservation_name);
+// mail ($to, $subject, $message, 'From: ' . $reservation_name);
+mail ($to, $subject, $message, 'From: ' . $reservation_email);
 header("Location: http://www.necto.com/bottle-service-and-reservation-thank-you.html");
 
 
