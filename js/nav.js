2783904c483002f1e2524a5ca15567780d326319
Christopher Collins <chris@spring215.com>
2016-09-16 17:59:44 -0400

    new nav js, changed index nav HTML

--- js/nav.js
+++ js/nav.js
@@ -1,46 +1,79 @@
 function nav()
 {
-	
+	// Find out if a page is in a subfolder and split into arr
+	var url = window.location;
+	var stringedURL = url.toString();
+	stringedURL = stringedURL.split('/');
+
+	// Sets the URl for each day. Change this if a day updates
+	var mondayURL   = 'factory-monday.html';
+	var thursdayURL = 'mix-thursday.html';
+	var fridayURL   = 'pride-friday.html';
+	var saturdayURL = 'frequency-saturday.html';
+
+	// Sets the URL for other pages
+	var bottleServiceURL = 'bottle-service-and-reservations.html'
+	var privEventURL = 'private-event-night-club-rental.html'
+
+
+	if (stringedURL.length <= 5) {
+		mondayURL   = mondayURL;
+		thursdayURL = thursdayURL;
+		fridayURL   = fridayURL;
+		saturdayURL = saturdayURL;
+		bottleServiceURL = bottleServiceURL;
+		privEventURL = privEventURL;
+	}
+
+	else if (stringedURL.length === 6) {
+		mondayURL   = '../' + mondayURL;
+		thursdayURL = '../' +  thursdayURL;
+		fridayURL   = '../' +  fridayURL;
+		saturdayURL = '../' +  saturdayURL;
+		bottleServiceURL = '../' + bottleServiceURL
+		privEventURL = '../' + privEventURL;
+	}
+
+	else if (stringedURL.length === 7) {
+		mondayURL   = '../../' + mondayURL;
+		thursdayURL = '../../' + thursdayURL;
+		fridayURL   = '../../' + fridayURL;
+		saturdayURL = '../../' + saturdayURL;
+		privEventURL = '../../' + privEventURL;
+	}
+
 
 	/* SET NUMBER OF COLUMNS (Must change if adding/removing a night) */
 	document.getElementById('globalNavLargeLis').className = 'small-block-grid-2 large-block-grid-4';
 
+	/* Weekly Event Links */
+	/* MONDAY */
+	document.getElementById('desktopMondayLink').href = mondayURL;
+	document.getElementById('mobileMondayLink').href=  mondayURL;
+	desktopMondayLink.insertAdjacentHTML('afterbegin', '<strong>FACTORY</strong><br>MONDAY');
+	mobileMondayLink.insertAdjacentHTML('afterbegin', 'FACTORY MONDAY');
+	/* THURSDAY*/
+	document.getElementById('desktopThursdayLink').href= thursdayURL;
+	document.getElementById('mobileThursdayLink').href= thursdayURL;
+	desktopThursdayLink.insertAdjacentHTML('afterbegin', '<strong>MIX</strong><br>THURSDAY');
+	mobileThursdayLink.insertAdjacentHTML('afterbegin', 'MIX THURSDAY');
+	/* FRIDAY */
+	document.getElementById('desktopFridayLink').href= fridayURL;
+	document.getElementById('mobileFridayLink').href= fridayURL;
+	desktopFridayLink.insertAdjacentHTML('afterbegin', '<strong>PRIDE</strong><br>FRIDAY');
+	mobileFridayLink.insertAdjacentHTML('afterbegin', 'PRIDE FRIDAY');
+	/* SATURDAY */
+	document.getElementById('desktopSaturdayLink').href= saturdayURL;
+	document.getElementById('mobileSaturdayLink').href= saturdayURL;
+	desktopSaturdayLink.insertAdjacentHTML('afterbegin', '<strong>FREQUENCY</strong><br>SATURDAY');
+	mobileSaturdayLink.insertAdjacentHTML('afterbegin', 'FREQUENCY SATURDAY');
 
-	/* ROOT LEVEL LINKS */
-
-	/* MONDAY FULL SCREEN */
-	document.getElementById('mondayNightFSroot').href='factory-monday.html';
-	mondayNightFSroot.insertAdjacentHTML('afterbegin', '<strong>FACTORY</strong><br>MONDAY');
-
-	/* THURSDAY FULL SCREEN */
-	document.getElementById('thursdayNightFSroot').href='college-night-thursday.html';
-	thursdayNightFSroot.insertAdjacentHTML('afterbegin', '<strong>COLLEGE NIGHT</strong><br>THURSDAY');
-
-	/* FRIDAY FULL SCREEN */
-	document.getElementById('fridayNightFSroot').href='pride-friday.html';
-	fridayNightFSroot.insertAdjacentHTML('afterbegin', '<strong>PRIDE</strong><br>FRIDAY');
-
-	/* SATURDAY FULL SCREEN */
-	document.getElementById('saturdayNightFSroot').href='frequency-saturday.html';
-	saturdayNightFSroot.insertAdjacentHTML('afterbegin', '<strong>FREQUENCY</strong><br>SATURDAY');
-
-
-
-	/* MONDAY FULL SCREEN */
-	document.getElementById('mondayNightFS2D').href='../factory-monday.html';
-	mondayNightFS2D.insertAdjacentHTML('afterbegin', '<strong>FACTORY</strong><br>MONDAY');
-
-	/* THURSDAY FULL SCREEN */
-	document.getElementById('thursdayNightFS2D').href='../college-night-thursday.html';
-	thursdayNightFS2D.insertAdjacentHTML('afterbegin', '<strong>COLLEGE NIGHT</strong><br>THURSDAY');
-
-	/* FRIDAY FULL SCREEN */
-	document.getElementById('fridayNightFS2D').href='../pride-friday.html';
-	fridayNightFS2D.insertAdjacentHTML('afterbegin', '<strong>PRIDE</strong><br>FRIDAY');
-
-	/* SATURDAY FULL SCREEN */
-	document.getElementById('saturdayNightFS2D').href='../frequency-saturday.html';
-	saturdayNightFS2D.insertAdjacentHTML('afterbegin', '<strong>FREQUENCY</strong><br>SATURDAY');
-
+	// Other Pages
+	// Bottle Service
+	document.getElementById('desktopBottleServiceURL').href = bottleServiceURL;
+	document.getElementById('mobileBottleServiceURL').href=  bottleServiceURL;
+	// Private Events
+	document.getElementById('desktopPrivEventsURL').href = privEventURL;
+	document.getElementById('mobilePrivEventsURL').href=  privEventURL;
 
 }
