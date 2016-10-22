dfb4143b1ff98e4c358706b0e446ca43a463fe7b
Christopher Collins <chris@spring215.com>
2016-10-22 11:29:49 -0400

    cal update, vip php change

--- renter_contact_from.php
+++ renter_contact_from.php
@@ -4,7 +4,7 @@ $renter_name = $_POST['renter_name'];
 $renter_phone = $_POST['renter_phone'];
 $renter_email = $_POST['renter_email'];
 $renter_message = $_POST['renter_message'];
-$to = 'info@necto.com';
+$to = 'vip@necto.com';
 $subject = 'Private Rental';
 
 $message = 'NAME: ' . $renter_name . "\n" . 'PHONE: ' . $renter_phone . "\n" . 'EMAIL: ' . $renter_email . "\n" . 'MESSAGE: ' . $renter_message;
--- reservation_contact_from.php
+++ reservation_contact_from.php
@@ -5,11 +5,15 @@ $reservation_phone = $_POST['reservation_phone'];
 $reservation_email = $_POST['reservation_email'];
 $reservation_date = $_POST['reservation_date'];
 $reservation_message = $_POST['reservation_message'];
-// $to = 'info@thenecto.com';
-$to = 'info@necto.com';
-$subject = 'Booth Rental or Bottle Service';
+$reservation_brand = $_POST['reservation_brand'];
+$reservation_guests = $_POST['reservation_guests'];
+$reservation_reffered = $_POST['reservation_reffered'];
 
-$message = 'NAME: ' . $reservation_name . "\n" . 'PHONE: ' . $reservation_phone . "\n" . 'EMAIL: ' . $reservation_email . "\n" . $reservation_date . "\n" . 'MESSAGE: ' . $reservation_message;
+// $to = 'info@thenecto.com';
+$to = 'vip@necto.com';
+$subject = 'VIP REQUEST';
+
+$message = 'NAME: ' . $reservation_name . "\n" . 'PHONE: ' . $reservation_phone . "\n" . 'EMAIL: ' . $reservation_email . "\n" . $reservation_date . "\n" . 'MESSAGE: ' . $reservation_message . "\n" . 'BRAND: ' . $reservation_brand . "\n" . 'REFFERED: ' . $reservation_reffered;
 
 // mail ($to, $subject, $message, 'From: ' . $reservation_name);
 mail ($to, $subject, $message, 'From: ' . $reservation_email);
