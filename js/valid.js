0e0dcf5ac5aa0c8a7388e7112f2ed951bd60e6ca
Christopher Collins <cacollins@cacollins-21616.local>
2017-02-28 11:40:47 -0500

    cleanded up valid()

--- js/valid.js
+++ js/valid.js
@@ -1,19 +1,16 @@
 function valid()
 {
-
 	var human =  document.getElementById('human');
 	
 	if (human.value == 'yes' | human.value == 'Yes' | human.value == 'YES' | human.value == 'Y' | human.value == 'y' | human.value == 'yup')
 		{
-			
+		
 			submit.type = 'submit';
 			document.getElementById("submit").disabled = false;
 			submit.value = "CONTACT US";
 		}
 	else
 		{
-
-
 			document.getElementById("submit").disabled = true;
 			submit.value = "PLEASE FILL OUT ALL FIELDS";
 		}
